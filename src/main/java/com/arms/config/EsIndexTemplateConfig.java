974dd2b4b08fa613bdf5842fcd3df711eb234fcd
sevoon0909 <sevoon0909@icloud.com>
2024-09-01 12:05:00 +0900

    fix: [Engine] #comment 서비스 재기동시에 기존에 존재하는 알리아스 변경 로직 추가 24.09.01 엘라스틱 서치에서 인덱스는 생성할때 기본적으로 알리아스를 생성을 하지만 호출하지 못하는 이름이 없는 알리아스를 기본적으로 만든다. 대표적인 예로 플루언트디는 암스에서 관리 하지 않는 인덱스이기 때문에 템플레이트도 없는 상태에서 이름이 없는 알리아스로 생성이된다. 다른 암스에서 관리하는 인덱스는 템플레이트가 기본적으로 생성이 되면서 해당 템플레이트에 알리아스의 이름을 명시하기 때문에 문제가없다. 따라서 플루언트 디는 알리아스 이름이 변경이 필요하기 때문에 해당 로직을 삽입함.
    #close #time 1h +review SR @mklee
    
    [ Engine-Fire::Java-Service-Tree-Framework(JSTF)::mklee ] [Requirement Manage] http://www.a-rms.net
    [Document] http://www.313.co.kr/confluence
    [IssueTracker] http://www.313.co.kr/jira
    [VersionControl] https://github.com/313devgrp
    [CodeReview] http://www.313.co.kr/fecru
    [CICD-Deploy] http://www.313.co.kr/jenkins
    [BuildManager] http://www.313.co.kr/spinnaker
    [ArtifactManager] http://www.313.co.kr/nexus
    [CodeAnalysis] http://www.313.co.kr/sonar
    [Docker Hub] https://hub.docker.com/u/313devgrp
    
    [Kubernetes] http://www.313.co.kr:31380
    [DockerSwarm] http://www.313.co.kr/portainer/#!/auth
    
    [DB Admin] http://www.313.co.kr/phpmyadmin/
    [S3 Admin] http://www.313.co.kr/minio/login
    [MEM Admin] http://www.313.co.kr/redis/
    [NoSql Index] http://www.313.co.kr/elasticsearch/_cat/indices [NoSql Node] http://www.313.co.kr/elasticsearch/_nodes?pretty=true [Kibana] http://www.313.co.kr/kibana/app/home#/
    [LogStash] http://www.313.co.kr/logstash/_node/stats?pretty [ZipKin] http://www.313.co.kr/zipkin/
    [ElasticHQ] http://www.313.co.kr/elastichq/#!/
    [Grafana] http://www.313.co.kr/grafana
    
    [NAS] http://www.313.co.kr:5000/webman/index.cgi
    [Mail] http://www.313.co.kr/mail/
    [Auth] http://www.313.co.kr/auth/
    [RDP] http://www.313.co.kr/guacamole/#/

--- src/main/java/com/arms/config/EsIndexTemplateConfig.java
+++ src/main/java/com/arms/config/EsIndexTemplateConfig.java
@@ -1,34 +1,52 @@
 package com.arms.config;
 
+import static com.arms.config.ApplicationContextProvider.*;
+
 import com.arms.egovframework.javaservice.esframework.annotation.ElasticSearchTemplateConfig;
 
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+
+import org.elasticsearch.action.admin.indices.alias.get.GetAliasesAction;
+import org.elasticsearch.action.admin.indices.alias.get.GetAliasesRequest;
+import org.elasticsearch.client.IndicesClient;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.client.indices.GetIndexRequest;
+import org.elasticsearch.client.indices.GetIndexResponse;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.ClassPathScanningCandidateComponentProvider;
 import org.springframework.core.annotation.AnnotationUtils;
 import org.springframework.core.type.filter.AnnotationTypeFilter;
 import org.springframework.data.elasticsearch.annotations.Document;
 import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
+import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
 import org.springframework.data.elasticsearch.core.index.AliasAction;
 import org.springframework.data.elasticsearch.core.index.AliasActionParameters;
 import org.springframework.data.elasticsearch.core.index.AliasActions;
+import org.springframework.data.elasticsearch.core.index.AliasData;
 import org.springframework.data.elasticsearch.core.index.PutTemplateRequest;
+import org.springframework.data.elasticsearch.core.mapping.IndexCoordinates;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
+
+import java.io.IOException;
 import java.lang.annotation.Annotation;
+import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Component
+@AllArgsConstructor
 @Slf4j
 public class EsIndexTemplateConfig {
 
 	private final ElasticsearchOperations operations;
 
-	public EsIndexTemplateConfig(ElasticsearchOperations operations) {
-		this.operations = operations;
-	}
 
 	@PostConstruct
 	public void run() {
@@ -54,15 +72,30 @@ public class EsIndexTemplateConfig {
 						log.info("template-{} 생성진행",templateName);
 						var mapping = indexOperations.createMapping();
 
-						var aliasActions = new AliasActions().add(
-							new AliasAction.Add(AliasActionParameters.builderForTemplate()
-								.withAliases(indexOperations.getIndexCoordinates().getIndexNames())
-								.build())
-						);
+						try(RestHighLevelClient client = getBean(RestHighLevelClient.class)) {
+							GetIndexResponse getIndexResponse = client.indices()
+								.get(new GetIndexRequest(document.indexName() + "*"), RequestOptions.DEFAULT);
+
+							AliasActions add = new AliasActions().add(
+								new AliasAction.Add(AliasActionParameters.builderForTemplate()
+									.withAliases(indexOperations.getIndexCoordinates().getIndexNames())
+									.withIndices(getIndexResponse.getIndices())
+									.build())
+							);
+
+							indexOperations.alias(add);
+
+						} catch (IOException e) {
+							throw new RuntimeException(e);
+						}
 
 						var request = PutTemplateRequest.builder(templateName, templatePattern)
 							.withMappings(mapping)
-							.withAliasActions(aliasActions)
+							.withAliasActions(new AliasActions().add(
+								new AliasAction.Add(AliasActionParameters.builderForTemplate()
+									.withAliases(indexOperations.getIndexCoordinates().getIndexNames())
+									.build())
+							))
 							.build();
 
 						indexOperations.putTemplate(request);
